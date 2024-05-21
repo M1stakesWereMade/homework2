69c22dee01574a633f99fc8e2ed73b7948f03f9d
M1stakesWereMade <superlexlive@gmail.com>
2024-05-21 14:29:54 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+Number_of_completed_HM = 12
+Number_of_hours_spent = 1.5
+Course_name = 'Python'
+Time_per_task = Number_of_hours_spent / Number_of_completed_HM
+print('Курс:',Course_name +',','всего задач:',Number_of_completed_HM,',','затрачено часов:',Number_of_hours_spent,',','среднее время выполнения:',Time_per_task,'часа')
